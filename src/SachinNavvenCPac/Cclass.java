f98fc9306c5d87196601a8e52aed7348064fdb12
sachinbasha <142664030+sachinbasha@users.noreply.github.com>
2024-04-30 16:29:49 +0530

    Update Cclass.java

--- src/SachinNavvenCPac/Cclass.java
+++ src/SachinNavvenCPac/Cclass.java
@@ -4,10 +4,7 @@ public class Cclass {
 
 	public static void main(String[] args) {
 		System.out.println("sachin");
-		System.out.println("amar");
-		System.out.println("akabar");
-		System.out.println("antony");
-		System.out.println("antoj");
+	
 		
 	}
 
